8535e6c988f4ad08082b3ac1634d82c26d830e53
AkiShark2569 <53553806+AkiShark2569@users.noreply.github.com>
2019-08-01 15:28:06 +0900

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+﻿import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("おはよう"):
+        if client.user != message.author:
+            m = "おはようございます" + message.author.name + "さん！"
+            await message.channel.send(m)
+
+client.run("NjA2MzY4NTkxOTEwMjczMDI0.XUKC6Q.fXw7x4PrbmFV0F8tD0EFf_DXIlo")
